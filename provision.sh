30bb4e51f1976769c9a8fb8a6ce1282eb2102ccb
Diego Chilet <diegochilet99@gmail.com>
2020-10-28 11:32:01 +0100

    default editado, línea de vagrant cambiada y box funcionando

--- provision.sh
+++ provision.sh
@@ -1,4 +1,6 @@
 apt-get -y update
 apt-get install -y nginx
+rm -rf /etc/nginx/sites-enabled
+cp -R /vagrant/sites-enabled /etc/nginx/sites-enabled 
+rm -rf "/usr/share/nginx/www"
 service nginx start
-cp -avr /usr/share/nginx/www /vagrant/www
